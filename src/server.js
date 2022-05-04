8df5b0ef40560a9ed14080b0a62a802cbd9ee565
Greta <gretaapul@gmail..com>
2022-05-04 09:10:41 +0300

    config server

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const { PORT } = require('./config');
+
+const app = express();
+
+// Middle ware
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => res.json('hello'));
+
+// Routes
+
+// 404
+
+app.all('*', (req, res) => {
+  res.statusCode(404).json({ error: 'Page not found' });
+});
+//
+
+app.listen(PORT, () => console.log('server is runnig on port', +PORT));
